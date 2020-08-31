aa9b57fd6d90b31365c191e5c16b3a9a09836e95
Henrique <henriquegundes@outlook.com>
2020-08-30 22:24:34 -0300

    First Module - GoStack

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+
+import Header from './components/Header';
+
+import api from './services/api';
+
+import './App.css';
+
+function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            setProjects(response.data);
+        })
+    }, []);
+
+    async function handleAddProject() {
+        const response =  await api.post('/projects', {
+            title: `Novo Projeto ${Date.now()}`,
+            owner: "Henrique"
+        });
+        const project = response.data;
+        setProjects([... projects, project]);
+    }
+    
+    return (
+        <>
+            <Header title="Projects"/>
+            <ul>
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
+            </ul>
+            <button
+                onClick={handleAddProject} 
+                type="button"
+            >
+                Adicionar projeto
+            </button>
+        </>
+    );
+}
+
+export default App;
